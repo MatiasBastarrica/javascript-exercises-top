7a5f38cbd3a8724e6db8052962d2c0eb065b6f91
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-05 07:59:21 -0300

    Complete Exercise 09 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  // let result;
+  let orderedRange = [num1, num2].sort((a, b) => a - b);
+  let rangeStart = orderedRange[0];
+  let rangeEnd = orderedRange[1];
 
+  if (
+    Number.isInteger(num1) &&
+    num1 > 0 &&
+    Number.isInteger(num2) &&
+    num2 > 0
+  ) {
+    let sumTotal = 0;
+    for (let i = rangeStart; i <= rangeEnd; i++) {
+      sumTotal += i;
+    }
+    return sumTotal;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
