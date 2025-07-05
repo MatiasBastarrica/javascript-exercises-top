093133b5f785210b2e2303d8f0526c66af0ec4f8
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-05 07:40:04 -0300

    Commplete Exercise 08 removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, a, b, c, d) {
+  let filtered;
+  filtered = array.filter(
+    (item) => !(item === a || item === b || item === c || item === d)
+  );
+  return filtered;
 };
 
 // Do not edit below this line
