695ce6486fc92654369e2f16783cd96a8c088789
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-07 22:15:08 -0300

    Complete Exercise fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let arr = [0, 1];
 
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 2; i <= +num; i++) {
+    arr[i] = arr[i - 2] + arr[i - 1];
+  }
+
+  return arr[+num];
 };
 
 // Do not edit below this line
