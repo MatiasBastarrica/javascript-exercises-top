8a1c34e33eed0c6ada6aa3d9db521d04284216fc
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-08 08:16:59 -0300

    Complete Exercise 16 findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let oldestBirthYear = arr.sort((a, b) => a.yearOfBirth - b.yearOfBirth);
 
+  if (oldestBirthYear[0].yearOfDeath === undefined) {
+    return oldestBirthYear[0];
+  }
+
+  let inAgeOrder = arr.sort((a, b) => {
+    let lifeSpanOfA = a.yearOfDeath - a.yearOfBirth;
+    let lifeSpanOfB = b.yearOfDeath - b.yearOfBirth;
+    if (!(lifeSpanOfB - lifeSpanOfA)) {
+      return 1;
+    }
+    return lifeSpanOfB - lifeSpanOfA;
+  });
+
+  return inAgeOrder[0];
 };
 
 // Do not edit below this line
