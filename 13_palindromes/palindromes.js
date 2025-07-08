3ad0bc294ecee87d6b24ab7f81a6e528caefd9b5
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-07 21:15:05 -0300

    Complete Exercise 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,40 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let punctuationMarks = [
+    ",",
+    ".",
+    "-",
+    "?",
+    "!",
+    ":",
+    ";",
+    "(",
+    ")",
+    "{",
+    "}",
+    "'",
+    '"',
+    "/",
+  ];
 
+  function isPunctuationMark(item) {
+    return punctuationMarks.includes(item);
+  }
+
+  function keepLettersOnly(string) {
+    let stringSplitted = string.split("");
+    let arrayWithLetters = stringSplitted.filter(
+      (item) => !isPunctuationMark(item) && item !== " "
+    );
+    return arrayWithLetters.join("").toLowerCase();
+  }
+
+  let stringFormatted = keepLettersOnly(string);
+  let stringFormattedMirrored = stringFormatted.split("").reverse().join("");
+  if (!stringFormatted.localeCompare(stringFormattedMirrored)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
