c34efdbd6e533fea58025e2a9e980f1e0f07860e
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-21 11:02:12 -0300

    Finish the factorial exercise

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function (num) {
+  if (num < 0 || typeof num !== "number" || !Number.isInteger(num)) {
+    return undefined;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
