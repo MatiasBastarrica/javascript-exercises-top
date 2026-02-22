e3037f0b1879b83363b14c48dc7d10bccf20abb3
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-21 21:48:08 -0300

    Finish the contains exercise

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,64 @@
-const contains = function() {
-  
+// const meaningOfLifeArray = [42];
+// const object = {
+//   data: {
+//     duplicate: "e",
+//     stuff: {
+//       thing: {
+//         banana: NaN,
+//         moreStuff: {
+//           something: "foo",
+//           answer: meaningOfLifeArray,
+//         },
+//       },
+//     },
+//     info: {
+//       duplicate: "e",
+//       magicNumber: 44,
+//       empty: null,
+//     },
+//   },
+// };
+
+// const contains = function (obj, value) {
+//   const values = Object.values(obj);
+//   for (let i = 0; i < values.length; i++) {
+//     if (Array.isArray(values[i])) {
+//       values[i].forEach((item) => {
+//         if (item === value) {
+//           return true;
+//         }
+//       });
+//     } else if (values[i] === value) {
+//       return true;
+//     } else if (typeof values[i] === "object" && values[i] !== null) {
+//       if (contains(values[i], value)) {
+//         return true;
+//       } else if (i === values.length - 1) {
+//         return false;
+//       }
+//     }
+//   }
+// };
+
+const contains = function (obj, value) {
+  const values = Object.values(obj);
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === value) {
+      return true;
+    } else if (Number.isNaN(values[i]) && Number.isNaN(value)) {
+      return true;
+    }
+    if (typeof values[i] === "object" && values[i] !== null) {
+      if (contains(values[i], value)) {
+        return true;
+      } else if (i === values.length - 1) {
+        return false;
+      }
+    }
+  }
 };
-  
+
+// contains(object, 44);
+
 // Do not edit below this line
 module.exports = contains;
