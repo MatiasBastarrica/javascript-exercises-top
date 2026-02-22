4981561bc57b3bfe59bbef35cd602e922d76a3d1
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-22 11:23:30 -0300

    Write the function totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,14 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (deep) {
+  let integerCount = 0;
+  const values = Object.values(deep);
+  const integerValues = values.filter((value) => Number.isInteger(value));
+  integerCount += integerValues.length;
+  const nestedObjects = values.filter((value) => {
+    return typeof value === "object" && value !== null;
+  });
+  nestedObjects.forEach((obj) => integerCount + totalIntegers(obj));
+  return integerCount;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
