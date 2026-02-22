e7440da6e2b64d7b25ee47a0884b2f7096c83d83
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-22 20:07:55 -0300

    Write and test permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,25 @@
-const permutations = function() {
-  
+const permutations = function (array) {
+  if (array.length === 0) {
+    return [[]];
+  } else if (array.length === 1) {
+    return [[1]];
+  }
+
+  let numToAdd = array[array.length - 1];
+
+  let prevPartitions;
+  prevPartitions = partitions(array.toSpliced(array.length - 1, 1));
+
+  let partitions = [];
+
+  prevPartitions.forEach((partition) => {
+    for (let i = 0; i < numToAdd; i++) {
+      partitions.push(partition.splice(i, 0, numToAdd));
+    }
+  });
+
+  return partitions;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
