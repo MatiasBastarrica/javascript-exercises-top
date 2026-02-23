5859140c6bee2cd5ed6d624c4895e6fc33f213c3
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-23 08:53:06 -0300

    Write the pascal function

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,21 @@
-const pascal = function() {
-  
+const pascal = function (rowNum) {
+  if (rowNum === 1) {
+    return [1];
+  }
+  let prevRow = pascal(rowNum - 1);
+
+  let prevRowWithZeros = [0, ...prevRow, 0];
+
+  let row = [];
+
+  for (let i = 0; i < prevRowWithZeros.length - 1; i++) {
+    const num = prevRowWithZeros[i];
+    const rightmostNum = prevRowWithZeros[i + 1];
+    row.push(num + rightmostNum);
+  }
+
+  return row;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
